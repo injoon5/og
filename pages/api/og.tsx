ec0647f23b3a3e7c6167d7467fa11e0f5684f741
Sunghyun Cho <mail@chosunghyun.com>
2023-04-08 21:11:58 -0700

    add pretendard

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,47 +1,55 @@
-import { ImageResponse } from '@vercel/og'
-import { NextRequest } from 'next/server'
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
 
 export const config = {
-  runtime: 'edge',
-}
+  runtime: "edge",
+};
+
+const fontSans = () =>
+  fetch(new URL(`https://og.cho.sh/Pretendard-ExtraBold.woff`)).then((res) =>
+    res.arrayBuffer()
+  )
 
 export default async function handler(req: NextRequest) {
+  const fontSansData = await fontSans()
   try {
-    const { searchParams } = new URL(req.url)
-    const hasTitle = searchParams.has('title')
+    const { searchParams } = new URL(req.url);
+    const hasTitle = searchParams.has("title");
     const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 150)
-      : 'The Latest Tech News 🗞️ in Your Language 💬 in Your Inbox 📭'
+      ? searchParams.get("title")?.slice(0, 150)
+      : "The Latest Tech News 🗞️ in Your Language 💬 in Your Inbox 📭";
 
-    const hasSubheading = searchParams.get('subheading')?.slice(0, 150)
-    const subheading = hasSubheading ? searchParams.get('subheading') : 'hn.cho.sh'
+    const hasSubheading = searchParams.get("subheading")?.slice(0, 150);
+    const subheading = hasSubheading
+      ? searchParams.get("subheading")
+      : "hn.cho.sh";
 
     return new ImageResponse(
       (
         <div
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
-            width: '100%',
-            padding: '20px 150px',
-            justifyContent: 'center',
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            width: "100%",
+            padding: "20px 150px",
+            justifyContent: "center",
             fontFamily: 'Pretendard, Inter, "Fluent Emoji"',
             fontSize: 60,
-            backgroundColor: '#c5daec',
-            backgroundImage: 'linear-gradient(9, #c5daec 0%, #ffffff 80%)',
+            backgroundImage: "linear-gradient(9, #151b36 0%, #000000 80%)",
             letterSpacing: -3,
             lineHeight: 1.2,
             fontWeight: 900,
-            wordWrap: 'break-word',
-            wordBreak: 'keep-all',
+            color: "white",
+            wordWrap: "break-word",
+            wordBreak: "keep-all",
           }}
         >
           <div
             style={{
               fontSize: 40,
-              color: 'gray',
-              margin: '20px 0',
+              color: "gray",
+              margin: "20px 0",
             }}
           >
             {subheading}
@@ -52,12 +60,20 @@ export default async function handler(req: NextRequest) {
       {
         width: 1200,
         height: 630,
+        fonts: [
+          {
+            name: 'Pretendard',
+            data: fontSansData,
+            style: 'normal',
+            weight: 700,
+          },
+        ],
       }
-    )
+    );
   } catch (e: any) {
-    console.log(`${e.message}`)
+    console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, {
       status: 500,
-    })
+    });
   }
 }
