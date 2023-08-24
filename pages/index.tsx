745aa442f6d4a4b19ad5a01ab5f4484d65adcd32
Sunghyun Cho <hey@cho.sh>
2023-08-23 23:14:37 -0700

    style: Improve styling and layout of OG Image Generator form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,98 @@
-import Head from "next/head";
 import { useState } from "react";
-import Image from "next/image";
 
 export default function Home() {
   const [title, setTitle] = useState("");
   const [subheading, setSubheading] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState(
+    "/api/og/?title=Generate%20Images%20on%20the%20Fly!&subheading=Hello%2C%20World!",
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setImageUrl(
       `/api/og/?title=${encodeURIComponent(
-        title
-      )}&subheading=${encodeURIComponent(subheading)}`
+        title,
+      )}&subheading=${encodeURIComponent(subheading)}`,
     );
     console.log(imageUrl);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div
+      style={{
+        maxWidth: 800,
+        margin: "0 auto",
+        display: "grid",
+        placeItems: "center",
+        fontFamily:
+          "'SF Pro', ui-sans-serif, -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo','Pretendard', 'Apple Color Emoji', 'Segoe UI Emoji', system-ui, -system-ui, sans-serif",
+      }}
+    >
+      <h1>OG Image Generator</h1>
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexDirection: "column", gap: 4 }}
+      >
         <label>
-          Subheading:
           <input
             type="text"
             value={subheading}
+            placeholder="Subheading"
+            style={{
+              fontSize: 20,
+              width: "100%",
+              margin: "1rem auto",
+              padding: 8,
+              borderRadius: 8,
+            }}
             onChange={(e) => setSubheading(e.target.value)}
           />
         </label>
-          <br />
+
         <label>
-          Title:
           <input
             type="text"
             value={title}
+            placeholder="Title"
+            style={{
+              fontSize: 40,
+              width: "100%",
+              margin: "1rem auto",
+              padding: 8,
+              borderRadius: 8,
+            }}
             onChange={(e) => setTitle(e.target.value)}
           />
         </label>
-        <br />
-        <button type="submit">Generate Image</button>
+
+        <button
+          type="submit"
+          style={{
+            fontFamily:
+              "'SF Pro', ui-sans-serif, -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo','Pretendard', 'Apple Color Emoji', 'Segoe UI Emoji', system-ui, -system-ui, sans-serif",
+            width: "30%",
+            padding: 8,
+            borderRadius: 8,
+            margin: "1rem auto",
+          }}
+        >
+          Generate Image
+        </button>
       </form>
       {imageUrl && (
-        <div>
+        <div
+          style={{
+            maxWidth: 800,
+            margin: "0 auto",
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
           <img
             src={imageUrl}
-            width={1200}
-            height={630}
+            style={{ width: "100%", height: "auto", margin: "20px 0" }}
             alt="Generated image"
           />
-        <code>{imageUrl}</code>
         </div>
       )}
     </div>
